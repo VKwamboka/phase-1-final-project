8038c1aa1cf510249f483ba627a1c26b20026f46
verah <verahnyagaka101@gmail.com>
2022-06-23 08:26:25 +0300

    api changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded",() => {
-    const jobsUrl = "http://localhost:3000/data"
+    // const jobsUrl = "http://localhost:3000/data"
 
     //job containers
     let jobContainer = document.getElementById("jobs")
@@ -9,19 +9,19 @@ document.addEventListener("DOMContentLoaded",() => {
     function displayJobs(itemJob){  
         const jobCard = `
         <div class="container">
-        <div id="jobs">
-        <h3 id="slug">${itemJob.slug}</h3>
-        <h4 id="company_name">${itemJob.company_name}</h4>
-        <h2 id="title">${itemJob.title}</h2>
-        <p id="description">${itemJob.description}</p>
-        <h3 id="remote">${itemJob.remote}</h3>
-        <p id="url">${itemJob.url}</p>
-        <h3 id="tags">${itemJob.tags}</h3>
-        <h3 id="job_types">${itemJob.job_types}</h3>
-        <h3 id="location">${itemJob.location}</h3>
-        <h4 id="created_at">${itemJob.created_at}</h4>
-    </div>
-    </div>
+         <div id="jobs">
+                <p id="slug">${itemJob.slug}</p>
+                <p id="company_name">Company: ${itemJob.company_name}</p>
+                <h4 id="title">${itemJob.title}</h4>
+                <p id="description">${itemJob.description}</p>
+                <p id="remote">${itemJob.remote}</p>
+                <p id="url">${itemJob.url}</p>
+                <p id="tags">${itemJob.tags}</p>
+                <p id="job_types">${itemJob.job_types}</p>
+                <p id="location">${itemJob.location}</p>
+                <p id="created_at">${itemJob.created_at}</p>
+            </div>
+        </div>
         `
         
         jobContainer.innerHTML = jobCard
@@ -30,20 +30,28 @@ document.addEventListener("DOMContentLoaded",() => {
     // get jobs
     function getJobs(){
         let reqOptions = {
-            method : "GET"
-        }
+            method : "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'X-RapidAPI-Key': 'f47f2d948amshf7e417382eb17b4p1b1341jsn86b1bb9c9915',
+                'X-RapidAPI-Host': 'arbeitnow-free-job-board.p.rapidapi.com'
+            }
+        };
         
-        fetch (jobsUrl, reqOptions)
+        fetch ('https://arbeitnow-free-job-board.p.rapidapi.com/api/job-board-api', reqOptions)
         .then((response) => response.json())
             // console.log(response)
         .then(results =>{
+            for(const jobbs in results){
+                displayJobs(jobbs)
+                console.log(jobbs)
+            }
+            // results.forEach(jobItem => {
+            //     displayJobs(jobItem)
+            //     // console.log(response[0])
+            // })
             
-            results.forEach(jobItem => {
-
-                displayJobs(jobItem)
-                
-            })
-        })
+        }) .catch(err => console.error(err));
       
     }
     getJobs()
